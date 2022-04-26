881b6122f0fffb4b5f1f276581ab0094b7d7a372
Robin Souriau <robin@diggersfactory.com>
2022-04-26 10:23:36 +0200

    Added zip_code and country

--- app/Services/User.js
+++ app/Services/User.js
@@ -591,7 +591,7 @@ User.getOrders = async (params) => {
 
 User.getOrderShop = async (params) => {
   const orderShop = await DB('order_shop as os')
-    .select('os.id', 'os.customer_id', 'os.user_id', 'c.address', 'os.address_pickup', 'c.city')
+    .select('os.id', 'os.customer_id', 'os.user_id', 'c.address', 'os.address_pickup', 'c.city', 'c.zip_code', 'c.country_id')
     .join('customer as c', 'c.id', 'os.customer_id')
     .where('order_id', +params.id)
     .first()
