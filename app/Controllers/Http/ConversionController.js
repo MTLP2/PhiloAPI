609ceb7709c2d85098856af1acfbaa90edc3d2ab
Robin Souriau <robin@diggersfactory.com>
2022-06-20 11:13:40 +0200

    Removed wrong branched commit

--- app/Controllers/Http/ConversionController.js
+++
@@ -1,9 +0,0 @@
-const Conversion = use('App/Services/Conversion')
-
-class ConversionController {
-  event ({ params }) {
-    return Conversion.event(params)
-  }
-}
-
-module.exports = ConversionController
