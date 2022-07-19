c03f9759a85dfdc22af5dbef7e8382cb28218d8b
Vico <vic.perin@gmail.com>
2022-07-19 09:55:17 +0200

    Check if statement already exists

--- app/Services/Statement.js
+++ app/Services/Statement.js
@@ -30,6 +30,14 @@ class StatementService {
     if (params.id) {
       item = await DB('statement').find(params.id)
     } else {
+      const exists = await DB('statement')
+        .where('project_id', params.project_id)
+        .where('date', params.year + '-' + params.month)
+        .first()
+
+      if (exists) {
+        return { error: 'statement_already_exists' }
+      }
       item.created_at = Utils.date()
     }
     item.project_id = params.project_id
