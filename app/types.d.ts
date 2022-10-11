66d7cc0bb132cf042237febe83e832bdfff1b2cd
Vico <vic.perin@gmail.com>
2022-10-11 16:29:39 +0200

    Add type for database models

---
+++ app/types.d.ts
@@ -0,0 +1,18 @@
+type Model = {
+  save(): Promise<boolean>
+  delete(): Promise<boolean>
+}
+
+type ChatDb = {
+  id: number
+  user_id: number
+  cookie_id: string
+  destination: string
+  is_diggers: boolean
+  text: string
+  seen: number
+  created_at: string
+  updated_at: string
+}
+
+type ChatModel = ChatDb & Model
