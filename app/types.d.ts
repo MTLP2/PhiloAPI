120b904fe14973993712d6d693fdf85270653508
Vico <vic.perin@gmail.com>
2022-10-20 10:30:16 +0200

    Set ShopDb fields nullable

--- app/types.d.ts
+++ app/types.d.ts
@@ -19,16 +19,16 @@ type ChatModel = ChatDb & Model
 
 type ShopDb = {
   id: number
-  code: string
-  name: string
-  bg_color: string
-  font_color: string
-  title_color: string
+  code: string | null
+  name: string | null
+  bg_color: string | null
+  font_color: string | null
+  title_color: string | null
   logo: string | null
   banner: string | null
   bg_image: string | null
-  created_at: string
-  updated_at: string
+  created_at: string | null
+  updated_at: string | null
 }
 
 type ShopModel = ShopDb & Model
