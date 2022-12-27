3528b42aafb5d6d93eb21d7d0d90c77ff41ff3a8
Vico <vic.perin@gmail.com>
2022-12-27 16:28:33 +0100

    Change line for SNA

--- app/Services/Quote.ts
+++ app/Services/Quote.ts
@@ -442,7 +442,7 @@ class Quote {
         'inner_sleeve'
       )
     } else if (params.inner_sleeve === 'printed') {
-      quote.inner_sleeve = getCost(119, 'inner_sleeve')
+      quote.inner_sleeve = getCost(123, 'inner_sleeve')
     } else {
       quote.inner_sleeve = getCost(
         {
@@ -512,53 +512,27 @@ class Quote {
         }
       } else {
         if (params.nb_vinyl === 1) {
-          if (params.quantity < 300) {
-            quote.sleeve =
-              getCost(
-                {
-                  '12"': 141,
-                  '10"': 141,
-                  '7"': 154
-                },
-                'sleeve',
-                ` x ${params.quantity}`
-              ) / params.nb_vinyl
-          } else {
-            quote.sleeve =
-              getCost(
-                {
-                  '12"': 146,
-                  '10"': 146,
-                  '7"': 154
-                },
-                'sleeve',
-                ` x ${params.quantity}`
-              ) / params.nb_vinyl
-          }
+          quote.sleeve =
+            getCost(
+              {
+                '12"': 141,
+                '10"': 141,
+                '7"': 154
+              },
+              'sleeve',
+              ` x ${params.quantity}`
+            ) / params.nb_vinyl
         } else {
-          if (params.quantity < 300) {
-            quote.sleeve =
-              getCost(
-                {
-                  '12"': 142,
-                  '10"': 142,
-                  '7"': 154
-                },
-                'sleeve',
-                ` x ${params.quantity}`
-              ) / params.nb_vinyl
-          } else {
-            quote.sleeve =
-              getCost(
-                {
-                  '12"': 148,
-                  '10"': 148,
-                  '7"': 154
-                },
-                'sleeve',
-                ` x ${params.quantity}`
-              ) / params.nb_vinyl
-          }
+          quote.sleeve =
+            getCost(
+              {
+                '12"': 142,
+                '10"': 142,
+                '7"': 154
+              },
+              'sleeve',
+              ` x ${params.quantity}`
+            ) / params.nb_vinyl
         }
       }
     }
@@ -601,9 +575,9 @@ class Quote {
     if (params.insert && params.insert !== 'none') {
       quote.insert = getCost(252, 'insert', ` x ${params.quantity}`) / params.nb_vinyl
       if (params.insert === 'two_sides_printed') {
-        quote.insert += getCost(368, 'insert', ` x ${params.quantity}`) / params.nb_vinyl
+        quote.insert += getCost(378, 'insert', ` x ${params.quantity}`) / params.nb_vinyl
       } else if (params.insert === 'one_side_printed') {
-        quote.insert += getCost(366, 'insert', ` x ${params.quantity}`) / params.nb_vinyl
+        quote.insert += getCost(376, 'insert', ` x ${params.quantity}`) / params.nb_vinyl
       } else if (params.insert === 'booklet_printed') {
         quote.insert += getCost(366, 'insert', ` x ${params.quantity}`) / params.nb_vinyl
       }
