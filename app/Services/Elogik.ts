5e8e6b3ff723a71d42ee1be70811969ffb037166
Vico <vic.perin@gmail.com>
2023-01-23 17:53:04 +0100

    Sync stock products

--- app/Services/Elogik.ts
+++ app/Services/Elogik.ts
@@ -572,70 +572,27 @@ class Elogik {
     })
     const news: any[] = []
 
+    const products = await DB('product')
+      .select('product.id', 'barcode')
+      .whereIn(
+        'barcode',
+        res.articles.map((r) => r.refEcommercant)
+      )
+      .all()
+
     for (const ref of res.articles) {
-      const project = await DB('project')
-        .select(
-          'project.id',
-          'project.name',
-          'project.artist_name',
-          'vod.barcode',
-          'u1.email as prod',
-          'u2.email as com'
-        )
-        .join('vod', 'vod.project_id', 'project.id')
-        .where('vod.barcode', ref.refEcommercant)
-        .leftJoin('user as u1', 'u1.id', 'vod.resp_prod_id')
-        .leftJoin('user as u2', 'u2.id', 'vod.com_id')
-        .first()
-
-      if (!project) {
-        // console.log('xxx not_found => ', ref.refEcommercant)
-        continue
-      }
-
-      const stock = await DB('stock')
-        .where('type', 'daudin')
-        .where('project_id', project.id)
-        .first()
-
-      const qty = ref.stocks[0].stockDispo
-
-      if (!stock && qty > 0) {
-        const emails = [
-          'ismail@diggersfactory.com',
-          'jean-baptiste@diggersfactory.com',
-          'romain@diggersfactory.com'
-        ]
-        if (project.com) {
-          emails.push(project.com)
-        }
-        if (project.prod) {
-          emails.push(project.prod)
-        }
-        console.log(`==> new stock : ${ref.refEcommercant} = ${qty}`)
-        await Notification.sendEmail({
-          to: emails.join(','),
-          subject: `Daudin - new stock : ${project.artist_name} - ${project.name}`,
-          html: `<ul>
-          <li><strong>Project:</strong> https://www.diggersfactory.com/sheraf/project/${project.id}/stocks</li>
-          <li><strong>Transporter:</strong> Daudin</li>
-          <li><strong>Barcode:</strong> ${project.barcode}</li>
-          <li><strong>Name:</strong> ${project.artist_name} - ${project.name}</li>
-          <li><strong>Quantity:</strong> ${qty}</li>
-        </ul>`
-        })
-        news.push({
-          ...project,
-          qty: qty
-        })
-      }
-
-      Stock.save({
-        project_id: project.id,
-        type: 'daudin',
-        comment: 'api',
-        quantity: qty
+      const product = products.find((p: any) => {
+        return p.barcode === ref.refEcommercant
       })
+
+      if (product) {
+        Stock.save({
+          product_id: product.id,
+          type: 'daudin',
+          comment: 'api',
+          quantity: ref.stocks[0].stockDispo
+        })
+      }
     }
 
     return news
