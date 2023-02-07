a778c36b23566d0306188c11549325fea41f6c93
Vico <vic.perin@gmail.com>
2023-02-07 17:05:01 +0100

    Fix problem with sticker

--- app/Services/Quote.ts
+++ app/Services/Quote.ts
@@ -635,7 +635,7 @@ class Quote {
     }
 
     // sticker
-    if (params.sticker !== '0') {
+    if (params.sticker && params.sticker !== '0') {
       quote.sticker = getCost(237, 'sticker', ` x ${params.quantity}`) / params.nb_vinyl
       quote.sticker += getCost(238, 'sticker', ` x ${params.quantity}`) / params.nb_vinyl
     }
