c0334c8fdbb889eaa91a801038bb0b708e5b3fc7
Vico <vic.perin@gmail.com>
2023-02-23 15:33:28 +0100

    Fix problem with picture project in catalog

--- app/Services/Admin.ts
+++ app/Services/Admin.ts
@@ -4009,6 +4009,7 @@ class Admin {
         'date_shipping',
         'product.barcode',
         'picture',
+        'picture_project',
         'project.cat_number',
         'product.catnumber',
         'inverse_name',
@@ -4122,7 +4123,9 @@ class Admin {
           : `https://www.diggersfactory.com/vinyl/${pp.id}/${pp.slug}?currency=${currency}${
               params.ori ? `&ori=${params.ori}` : ''
             };`
-      csv += `${Env.get('STORAGE_URL')}/projects/${pp.picture || pp.id}/vinyl.png;`
+      csv += pp.picture_project
+        ? `${Env.get('STORAGE_URL')}/projects/${pp.picture || pp.id}/${pp.picture_project}.png;`
+        : `${Env.get('STORAGE_URL')}/projects/${pp.picture || pp.id}/vinyl.png;`
       csv += `"${pp.artist_name}";`
       csv += ';;;;'
       csv += pp.estimated_shipping + ';'
