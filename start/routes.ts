5c12935c86da480d99056b5684fe6e5c7df68f4e
digggers <digggers@MacBook-Air-de-digggers.local>
2023-05-30 15:51:33 +0200

    add following method

--- start/routes.ts
+++ start/routes.ts
@@ -258,6 +258,9 @@ Route.group(() => {
   Route.get('/pass/quest-progress', 'UserController.getPassQuestProgress')
   Route.get('/pass/badge-progress', 'UserController.getPassBadgeProgress')
   Route.post('/pass/gift/:id/claim', 'UserController.claimGift')
+
+  Route.get('/follows', 'UserController.getFollows')
+  Route.post('/follows/:id', 'UserController.follow')
 })
   .prefix('user')
   .middleware(['auth'])
