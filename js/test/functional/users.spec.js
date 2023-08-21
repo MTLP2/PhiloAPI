7accedaba69e70b208ec6a8be3daf0299b1b54fe
Vico <vic.perin@gmail.com>
2023-08-21 14:40:35 +0200

    Remove JS folder

--- js/test/functional/users.spec.js
+++
@@ -1,9 +0,0 @@
-const { test, trait } = use('Test/Suite')('Users')
-
-trait('Test/ApiClient')
-
-test('get /users/1', async ({ client, assert }) => {
-  const response = await client.get('/users/1').end()
-  response.assertStatus(200)
-  assert.equal(response.body.error, undefined, response.body.error)
-})
