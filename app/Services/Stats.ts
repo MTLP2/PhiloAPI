895ee5f13f800336c0774aa63182eea09f44ff19
Vico <vic.perin@gmail.com>
2023-11-15 10:41:16 +0100

    Remove double turnover for invoice

--- app/Services/Stats.ts
+++ app/Services/Stats.ts
@@ -2442,7 +2442,6 @@ class Stats {
         d.sent.total.dates[date] += dis.total / currencies[stat.currency]
 
         addMarge('distrib', stat.is_licence ? 'licence' : 'project', date, marge)
-        addTurnover('invoice', 'distrib', stat.is_licence ? 'licence' : 'project', date, dis.total)
 
         if (!d.distrib.list[dis.name]) {
           d.distrib.list[dis.name] = {
