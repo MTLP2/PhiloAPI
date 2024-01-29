f024be8622f61c7f04d554e276cbca65d8b0ecb7
Vico <vic.perin@gmail.com>
2024-01-29 15:34:48 +0100

    Add Revolut service

--- app/Services/Revolut.ts
+++ app/Services/Revolut.ts
@@ -1,32 +1,37 @@
-import request from 'request'
 import Env from '@ioc:Adonis/Core/Env'
+const axios = require('axios')
 
 class Revolut {
   static api = (
     url: string,
     params?: {
       method?: string
+      data?: any
     }
   ) => {
-    return new Promise((resolve, reject) => {
-      request(
-        {
-          method: params?.method || 'GET',
-          url: `https://sandbox-b2b.revolut.com/api/1.0/${url}`,
-          // url: `https://merchant.revolut.com/api/1.0/${url}`,
-          json: true,
-          headers: {
-            Authorization: `Bearer ${Env.get('REVOLUT_SECRET')}`
-          },
-          body: params
-        },
-        function (err, res, body) {
-          console.log(res.statusCode)
-          if (err) reject(err)
-          else resolve(body)
-        }
-      )
-    })
+    console.log(Env.get('REVOLUT_SECRET_KEY'))
+    const config = {
+      method: params?.method || 'get',
+      maxBodyLength: Infinity,
+      url: Env.get('REVOLUT_URL') + url,
+      headers: {
+        'Content-Type': 'application/json',
+        'Revolut-Api-Version': '2023-09-01',
+        'Accept': 'application/json',
+        'Authorization': `Bearer ${Env.get('REVOLUT_SECRET_KEY')}`
+      },
+      data: params?.data ? JSON.stringify(params.data) : undefined
+    }
+
+    console.log(config)
+
+    return axios(config)
+      .then((res) => {
+        return res.data
+      })
+      .catch((error) => {
+        return error.response.data
+      })
   }
 
   static getOrders = async () => {
@@ -34,37 +39,22 @@ class Revolut {
     return res
   }
 
-  static createOrder = () => {
-    const axios = require('axios')
-    let data = JSON.stringify({
-      amount: 500,
-      currency: 'EUR'
-    })
+  static getOrder = async (id: string) => {
+    const res = await Revolut.api(`orders/${id}`)
+    return res
+  }
 
-    let config = {
+  static createOrder = (params: { amount: number; currency?: string }) => {
+    // 65b7662c-f7cd-ae8f-87e1-0d2772a0343c
+    // 286111b4-0aaf-4999-bd71-dc7f73d4b3a7
+
+    return Revolut.api('orders', {
       method: 'post',
-      maxBodyLength: Infinity,
-      url: 'https://sandbox-merchant.revolut.com/api/orders',
-      headers: {
-        'Content-Type': 'application/json',
-        'Revolut-Api-Version': '2023-09-01',
-        'Accept': 'application/json',
-        'Authorization':
-          'Bearer sk_XIKez0hHVKSy9lT8YYFVwJ2bgapGXdQ4r20YNFbWFlWLEAHc7xENM1NdcR1upeK4'
-      },
-      data: data
-    }
-
-    return axios(config)
-      .then((response) => {
-        console.log('0')
-        return response.data
-      })
-      .catch((error) => {
-        console.log('1')
-        console.log(error.response.data)
-        return error.response.data
-      })
+      data: {
+        amount: params.amount * 100,
+        currency: params.currency || 'EUR'
+      }
+    })
   }
 }
 
