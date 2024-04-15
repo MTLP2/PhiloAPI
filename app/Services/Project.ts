96afe2b8ca349a7cf64750a279e711eff557086b
Vico <vic.perin@gmail.com>
2024-04-15 12:35:02 +0200

    Fix shipping_discount round

--- app/Services/Project.ts
+++ app/Services/Project.ts
@@ -257,7 +257,7 @@ class Project {
 
       project.prices_ship_discount = project.shipping_discount
         ? Object.keys(project.prices).reduce((acc, key) => {
-            acc[key] = project.prices[key] + project.shipping_discount
+            acc[key] = Utils.round(project.prices[key] + project.shipping_discount)
             return acc
           }, {})
         : null
