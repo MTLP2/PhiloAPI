87185f905659d61327b99a1c9a2b7ed798488ca9
Vico <vic.perin@gmail.com>
2024-04-17 18:42:10 +0200

    Add multiple related id

--- app/Services/Admin.ts
+++ app/Services/Admin.ts
@@ -766,55 +766,58 @@ class Admin {
   }
 
   static saveProjectItem = async (params) => {
-    let item = DB('item')
+    const ids = params.related_id.toString().split(',')
+    for (const id of ids) {
+      let item = DB('item')
 
-    if (params.id) {
-      item = await DB('item').find(params.id)
-    } else {
-      const exists = await DB('item')
-        .where('project_id', params.project_id)
-        .where('related_id', params.related_id)
-        .first()
+      if (params.id) {
+        item = await DB('item').find(params.id)
+      } else {
+        const exists = await DB('item')
+          .where('project_id', params.project_id)
+          .where('related_id', id)
+          .first()
 
-      if (exists) {
-        return { error: 'already_exists' }
+        if (exists) {
+          return { error: 'already_exists' }
+        }
+
+        item.created_at = Utils.date()
       }
+      item.project_id = params.project_id
+      item.related_id = id || null
+      item.name = params.name || null
+      item.name_fr = params.name_fr || null
+      item.description_fr = params.description_fr || null
+      item.description_en = params.description_en || null
+      item.price = params.price || null
+      item.stock = params.stock || null
+      item.barcode = params.barcode || null
+      item.catnumber = params.catnumber || null
+      item.transporter = params.transporter || null
+      item.is_active = params.is_active
+      item.is_statement = params.is_statement
+      item.coefficient = params.coefficient || 1
+      item.weight = params.weight || 1
+      item.group_shipment = params.group_shipment
+      item.is_recommended = params.is_recommended
 
-      item.created_at = Utils.date()
-    }
-    item.project_id = params.project_id
-    item.related_id = params.related_id || null
-    item.name = params.name || null
-    item.name_fr = params.name_fr || null
-    item.description_fr = params.description_fr || null
-    item.description_en = params.description_en || null
-    item.price = params.price || null
-    item.stock = params.stock || null
-    item.barcode = params.barcode || null
-    item.catnumber = params.catnumber || null
-    item.transporter = params.transporter || null
-    item.is_active = params.is_active
-    item.is_statement = params.is_statement
-    item.coefficient = params.coefficient || 1
-    item.weight = params.weight || 1
-    item.group_shipment = params.group_shipment
-    item.is_recommended = params.is_recommended
-
-    if (params.picture) {
-      if (item.picture) {
-        Storage.deleteImage(item.picture)
+      if (params.picture) {
+        if (item.picture) {
+          Storage.deleteImage(item.picture)
+        }
+        const fileName = `items/${Utils.uuid()}`
+        item.picture = fileName
+        item.picture_trans = 1
+        Storage.uploadImage(fileName, Buffer.from(params.picture, 'base64'), {
+          type: 'png',
+          width: 800
+        })
       }
-      const fileName = `items/${Utils.uuid()}`
-      item.picture = fileName
-      item.picture_trans = 1
-      Storage.uploadImage(fileName, Buffer.from(params.picture, 'base64'), {
-        type: 'png',
-        width: 800
-      })
-    }
-    item.updated_at = Utils.date()
+      item.updated_at = Utils.date()
 
-    await item.save()
+      await item.save()
+    }
     return { success: true }
   }
 
