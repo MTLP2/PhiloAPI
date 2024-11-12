e2a87c2d42f5294a1cc68de76e91065e5b4317f5
Vico <vic.perin@gmail.com>
2024-11-12 09:47:29 +0100

    Add number validation

--- app/Controllers/Http/ProductsController.ts
+++ app/Controllers/Http/ProductsController.ts
@@ -59,7 +59,7 @@ class ProductsController {
           hs_code: schema.string.optional(),
           parent_id: schema.number.optional(),
           bigblue_id: schema.string.optional(),
-          whiplash_id: schema.string.optional(),
+          whiplash_id: schema.number.optional(),
           size: schema.string.optional(),
           color: schema.string.optional(),
           weight: schema.number.optional()
