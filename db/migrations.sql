1052e3da81b8060ffcf1fdc7bbb519197f31c4bf
Vico <vic.perin@gmail.com>
2025-04-03 10:47:51 +0200

    Add description_top field to vod and ensure surcharge_amount is correctly defined

--- db/migrations.sql
+++ db/migrations.sql
@@ -3,3 +3,6 @@ ALTER TABLE `order` ADD `qty_reserved` TINYINT(1) NULL DEFAULT NULL AFTER `is_gi
 
 # 2025-03-26 Add surcharge_amount to production for know if the type of surcharge is selected
 ALTER TABLE `production` ADD `surcharge_amount` VARCHAR(255) NULL DEFAULT NULL AFTER `is_billing`;
+
+# 2025-04-02 Add description_top to vod
+ALTER TABLE `vod` ADD `description_top` TEXT NULL DEFAULT NULL AFTER `description_fr_long`;
